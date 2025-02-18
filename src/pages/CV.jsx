bc3b690e8ad9b7f231ee99142509fb20e15f8103
L-stal <stalenhag.leo@gmail.com>
2025-02-18 12:40:14 +0100

    CV fix

--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -1,16 +1,16 @@
-import cvPdf from "/public/LeoStalenhag.pdf";
-
 const CV = () => {
   return (
-    <>
-      <div className="main-cv" id="cv">
-        <div className="shimmer">
-          <a href={cvPdf} target="_blank">
-            &gt; Resume
-          </a>
-        </div>
+    <div className="main-cv" id="cv">
+      <div className="shimmer">
+        <a
+          href="/Portfolio/LeoStalenhag.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          &gt; Resume
+        </a>
       </div>
-    </>
+    </div>
   );
 };
 
